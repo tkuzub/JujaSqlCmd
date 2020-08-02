9f094da4059056567a1c9432ba962661da307347
Taras Kuzub <taras.kuzub.ua@gmail.com>
2020-08-02 07:33:20 +0300

    added terminal command 'unsupported'

--- src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
+++ src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
@@ -3,7 +3,6 @@ package ua.com.juja.sqlcmd.controller;
 import ua.com.juja.sqlcmd.controller.command.Command;
 import ua.com.juja.sqlcmd.controller.command.Exit;
 import ua.com.juja.sqlcmd.controller.command.Help;
-import ua.com.juja.sqlcmd.model.DataSet;
 import ua.com.juja.sqlcmd.model.DatabaseManager;
 import ua.com.juja.sqlcmd.view.View;
 
@@ -19,25 +18,21 @@ public class MainController {
                 new Tables(manager, view),
                 new Help(view),
                 new Exit(view),
-                new Find(manager, view)};
+                new Find(manager, view),
+                new Unsupported(view)};
     }
 
     public void run() {
         connectToDB();
         while (true) {
             view.write("Enter an existing command (or command 'help' for help)");
-            String command = view.read();
+            String input = view.read();
 
-            if (commands[0].canProcess(command)) {
-                commands[0].process(command);
-            } else if (commands[1].canProcess(command)) {
-                commands[1].process(command);
-            } else if (commands[2].canProcess(command)) {
-                commands[2].process(command);
-            } else if (commands[3].canProcess(command)) {
-                commands[3].process(command);
-            } else {
-                view.write("non-existent command!!!");
+            for (Command command : commands) {
+                if (command.canProcess(input)) {
+                    command.process(input);
+                    break;
+                }
             }
         }
     }
---
+++ src/main/java/ua/com/juja/sqlcmd/controller/Unsupported.java
@@ -0,0 +1,22 @@
+package ua.com.juja.sqlcmd.controller;
+
+import ua.com.juja.sqlcmd.controller.command.Command;
+import ua.com.juja.sqlcmd.view.View;
+
+public class Unsupported implements Command {
+    private final View view;
+
+    public Unsupported(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public boolean canProcess(String command) {
+        return true;
+    }
+
+    @Override
+    public void process(String command) {
+        view.write("non-existent command " + "'" + command + "'");
+    }
+}
